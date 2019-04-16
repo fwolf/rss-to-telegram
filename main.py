ea4cea949c9a807efb283b0ad89966d35adebf31
Fwolf <fwolf.aide+git@gmail.com>
2019-04-16 13:04:16 +0800

    Fix html tag removal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # _*_ coding: utf-8 _*_
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, NavigableString
 from datetime import datetime
 from dateutil.parser import parse
 from config import config
@@ -12,6 +12,33 @@ import os
 
 def get_timestamp(dt):
     return parse(dt).timestamp()
+
+
+# Ref: https://stackoverflow.com/a/9045719/1759745
+KEEP_ATTRIBUTES = [
+    'href'
+]
+
+
+def clean_tags(tag):
+    if NavigableString == type(tag):
+        return tag
+
+    # 递归，先处理子结点，不然 unwrap 后本结点就变性了
+    for child in tag.contents :
+        clean_tags(child)
+
+    if tag.name not in ['b', 'i', 'a', 'code', 'pre']:
+        tag.unwrap()
+
+    if tag.name == 'a' :
+        allowed_attrs = {}
+        for key in tag.attrs :
+            if key in KEEP_ATTRIBUTES :
+                allowed_attrs[key] = tag.attrs[key]
+        tag.attrs = allowed_attrs
+
+    return tag
 
 
 bot = telebot.TeleBot(config.get('telegram-token'))
@@ -43,7 +70,12 @@ for feed in feeds:
         soup = BeautifulSoup(content, 'lxml')
         if soup.img:
             post['image'] = soup.img['src']
-        post['text'] = prefix + '\n'.join(soup.stripped_strings)
+
+        clean_tags(soup.html.body)
+
+        post['text'] = prefix
+        for part in soup.html.contents :
+            post['text'] += str(part) + '\n'
         post['text'] += '\n\n{}'.format(entry['link'])
         post['date'] = get_timestamp(entry['published'])
 
@@ -55,7 +87,12 @@ for feed in feeds:
 new_posts.sort(key=lambda x: x['date'])
 
 for post in new_posts:
-    bot.send_message(config.get('channel-id'), post.get('text'))
+    bot.send_message(
+        config.get('channel-id'),
+        post.get('text'),
+        parse_mode='HTML',
+        disable_web_page_preview=True
+        )
 
 with open('posts.json', 'w') as f:
     json.dump(guids, f)
