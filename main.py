b7284fde7155ad5c3681ef659e2ddab1720552a7
Fwolf <fwolf.aide+git@gmail.com>
2019-04-16 16:07:59 +0800

    Implement send of medias

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup, NavigableString
 from datetime import datetime
 from dateutil.parser import parse
 from config import config
+from pymediainfo import MediaInfo
+from telebot.types import InputMediaAnimation, InputMediaPhoto, InputMediaVideo
 import feedparser
 import telebot
 import json
@@ -41,6 +43,16 @@ def clean_tags(tag):
     return tag
 
 
+def is_video_has_sound(src):
+    media_info = MediaInfo.parse(src)
+    hasSound = False
+    for track in media_info.tracks:
+        if 'Audio' == track.track_type :
+            hasSound = True
+
+    return hasSound
+
+
 bot = telebot.TeleBot(config.get('telegram-token'))
 
 feeds = list()
@@ -69,9 +81,12 @@ for feed in feeds:
         content = content.replace('<br>', '\n').replace('<br />', '\n')
 
         post = {}
+        post['images'] = []
+        post['gifs'] = []
+        post['videos'] = []
         soup = BeautifulSoup(content, 'lxml')
         if soup.img:
-            post['image'] = soup.img['src']
+            post['images'].append(soup.img['src'])
 
         clean_tags(soup.html.body)
 
@@ -86,15 +101,64 @@ for feed in feeds:
             new_posts.append(post)
             guids.append(entry['guid'])
 
+        # Find images
+        # video: <link rel="enclosure" type="video/mp4" length="2365692" href="https://cmx.social/system/media_attachments/files/001/901/934/original/812bfbf669d03cec.mp4"/>
+        #   gif: <link rel="enclosure" type="video/mp4" length="20395" href="https://cmx.social/system/media_attachments/files/001/901/927/original/314f914a5ac552f1.mp4"/>
+        for link in entry.links :
+            if 'enclosure' == link.rel :
+                if ('image' == link.type[0:5]) :
+                    post['images'].append(link.href)
+                elif ('video' == link.type[0:5]) :
+                    if (is_video_has_sound(link.href)) :
+                        post['videos'].append(link.href)
+                    else :
+                        post['gifs'].append(link.href)
+
+
 new_posts.sort(key=lambda x: x['date'])
 
 for post in new_posts:
-    bot.send_message(
-        config.get('channel-id'),
-        post.get('text'),
-        parse_mode='HTML',
-        disable_web_page_preview=True
-        )
+    chat_id = config.get('channel-id')
+    caption = ''
+    if (1 < (len(post['images']) + len(post['videos']))) :
+        bot.send_message(
+            chat_id,
+            post.get('text'),
+            parse_mode='HTML',
+            disable_web_page_preview=True
+            )
+
+        medias = []
+        for image in post['images'] :
+            medias.append(InputMediaPhoto(image))
+        for video in post['videos'] :
+            medias.append(InputMediaVideo(video))
+        bot.send_media_group(chat_id, medias)
+
+    else :
+        if (1024 < len(post.get('text').encode('utf8'))) :
+            bot.send_message(
+                chat_id,
+                post.get('text'),
+                parse_mode='HTML',
+                disable_web_page_preview=True
+                )
+        else :
+            caption = post.get('text')
+
+            for image in post['images'] :
+                bot.send_photo(chat_id, image, caption=caption, parse_mode='HTML')
+                caption = ''
+            for video in post['videos'] :
+                bot.send_video(chat_id, video, caption=caption, parse_mode='HTML')
+                caption = ''
+
+# PR: https://github.com/eternnoir/pyTelegramBotAPI/pull/620 not approved
+    for gif in post['gifs'] :
+#        bot.send_animation(chat_id, gif, caption=caption, parse_mode='HTML')
+        bot.send_document(chat_id, gif, caption=caption, parse_mode='HTML')
+        caption = ''
+
 
 with open('posts.json', 'w') as f:
     json.dump(guids, f)
